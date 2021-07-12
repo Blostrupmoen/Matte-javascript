56ce587b4e459cb94d53dc6719111ff26c6909c1
Blostrupmoen <75287831+Blostrupmoen@users.noreply.github.com>
2021-07-12 16:13:56 +0200

    Changes to js
    Simplified js

--- index.js
+++ index.js
@@ -1,112 +1,49 @@
-// random num 1<10
-let randomNum = Math.floor(Math.random() * 10) + 1; 
-// num multiplied with random num.
-let num;
+document.querySelector('section.buttons').addEventListener('click', function (ev) {
+  if (ev.target.getAttribute("times-table") == null) {
+    return;
+  }
+  let randomNum = Math.floor(Math.random() * 10) + 1;
+  let timesTable = ev.target.getAttribute("times-table");
+  if (timesTable == 0) {
+    document.getElementById('toggleRandom').value = "true";
+  } else {
+    document.getElementById('toggleRandom').value = "false";
+  }
 
-
-
-
-
-function times1() {
-  let randomNum = Math.floor(Math.random() * 10) + 1; 
-  num = 1;
-  document.getElementById('task').innerHTML = randomNum  + " " +  "x" + " " +  num
-  return num * randomNum;
-  
-}
-
-
-function times2() {
-  let randomNum = Math.floor(Math.random() * 10) + 1; 
-  num = 2;
-  document.getElementById('task').innerHTML = randomNum  + " " +  "x" + " " +  num
-  return num * randomNum;
-
-}
-
-function times3() {
-  let randomNum = Math.floor(Math.random() * 10) + 1; 
-  num = 3;
-  document.getElementById('task').innerHTML = randomNum  + " " +  "x" + " " +  num
-  return num * randomNum;
-
-}
-
-function times4() {
-  let randomNum = Math.floor(Math.random() * 10) + 1; 
-  num = 4;
-  document.getElementById('task').innerHTML = randomNum  + " " +  "x" + " " +  num
-  return num * randomNum;
-
-}
-
-function times5() {
-  let randomNum = Math.floor(Math.random() * 10) + 1; 
-  num = 5;
-  document.getElementById('task').innerHTML = randomNum  + " " +  "x" + " " +  num
-  return num * randomNum;
-
-}
-
-function times6() {
-  let randomNum = Math.floor(Math.random() * 10) + 1; 
-  num = 6;
-  document.getElementById('task').innerHTML = randomNum  + " " +  "x" + " " +  num
-  return num * randomNum;
-
-}
-
-function times7() {
-  let randomNum = Math.floor(Math.random() * 10) + 1; 
-  num = 7;
-  document.getElementById('task').innerHTML = randomNum  + " " +  "x" + " " +  num
-  return num * randomNum;
-
-}
-
-function times8() {
-  let randomNum = Math.floor(Math.random() * 10) + 1; 
-  num = 8;
-  document.getElementById('task').innerHTML = randomNum  + " " +  "x" + " " +  num
-  return num * randomNum;
-
-}
-
-function times9() {
-  let randomNum = Math.floor(Math.random() * 10) + 1; 
-  num = 9;
-  document.getElementById('task').innerHTML = randomNum  + " " +  "x" + " " +  num
-  return num * randomNum;
-
-}
-
-function times10() {
-  let randomNum = Math.floor(Math.random() * 10) + 1; 
-  num = 10;
-  document.getElementById('task').innerHTML = randomNum  + " " +  "x" + " " +  num
-  return num * randomNum;
-  
-}
-
-
-function timesR() {
-  let randomNum = Math.floor(Math.random() * 10) + 1; 
-  let randomNum1 = Math.floor(Math.random() * 10) + 1; 
-  document.getElementById('task').innerHTML = randomNum1  + " " +  "x" + " " +  randomNum
-  return randomNum1 * randomNum;
-}
-
-
+  setUpTask(randomNum, timesTable);
+});
 
 
 function getAnswer() {
+  let a = document.getElementById('task').innerHTML;
+  let multipliedNumber = a.split(" x ");
   let input1 = document.getElementById("inputAnswer").value;
-  let sum = num * randomNum;
-  if (input1 == sum ) {
-  alert("Det er riktig!");
-  } else {
-  alert("Det er feil")
-  }
-  console.log(input1);
+
+  if (input1 == multipliedNumber[0] * multipliedNumber[1]) {
+    alert("Det er riktig!");
+
+    document.getElementById("inputAnswer").value = "";
   
-}  
+    let randomNum = Math.floor(Math.random() * 10) + 1;
+    let timesTable = multipliedNumber[1];
+    setUpTask(randomNum, timesTable);
+
+    document.getElementById("streak").innerHTML += '<i class="bi bi-star-fill"></i>';
+
+    
+
+
+  }
+  else {
+    alert("Det er feil")
+    document.getElementById("streak").innerHTML = "";
+  }
+}
+
+
+function setUpTask(randomNum, timesTableNum) {
+  if (document.getElementById('toggleRandom').value == "true") {
+    timesTableNum = Math.floor(Math.random() * 10) + 1;
+  }
+  document.getElementById('task').innerHTML = randomNum + " " + "x" + " " + timesTableNum;
+}
